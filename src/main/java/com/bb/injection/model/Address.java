822d768c0dae6331efc893cacd849fc8657e8e2b
Ankit Kapoor <akapoor@xebia.com>
2016-05-26 15:33:31 +0530

    Initial commit

---
+++ src/main/java/com/bb/injection/model/Address.java
@@ -0,0 +1,8 @@
+package com.bb.injection.model;
+
+/**
+ * Created by ankit on 25/5/16.
+ */
+public class Address {
+
+}
---
+++ src/main/java/com/bb/injection/model/User.java
@@ -0,0 +1,7 @@
+package com.bb.injection.model;
+
+/**
+ * Created by ankit on 25/5/16.
+ */
+public class User {
+}
