8a9d470a7fa47e31a4ff0540e0b3202473bf5e89
Ankit Kapoor <akapoor@xebia.com>
2016-06-01 18:14:45 +0530

    Added getting of address code

--- src/main/java/com/bb/injection/Service/AddressService.java
+++ src/main/java/com/bb/injection/Service/AddressService.java
@@ -1,6 +1,7 @@
 package com.bb.injection.Service;
 
 import com.bb.injection.dao.AddressDao;
+import com.bb.injection.model.Address;
 
 import java.util.List;
 
@@ -8,7 +9,7 @@ import java.util.List;
  * Created by ankit on 31/5/16.
  */
 public class AddressService {
-
+    private static AddressDao addressDao=AddressDao.getInstance();
     private static AddressService addressService =new AddressService();
     private AddressService(){}
     public static AddressService getInstance(){
@@ -16,8 +17,12 @@ public class AddressService {
     }
     //TODO UsersId are fixed right now make them dynamic
     public void updateAddress(){
-        AddressDao addressDao=new AddressDao();
+
         List<Long> addressIds=addressDao.getAddressId();
         addressDao.updateAddress(addressIds);
+    }
+
+    public List<Address> getAddress(){
+        return addressDao.getAddresses();
     }
 }
--- src/main/java/com/bb/injection/dao/AddressDao.java
+++ src/main/java/com/bb/injection/dao/AddressDao.java
@@ -2,6 +2,8 @@ package com.bb.injection.dao;
 
 import com.bb.injection.constants.Constants;
 import com.bb.injection.model.Address;
+import com.bb.injection.model.Country;
+import com.bb.injection.model.Region;
 import com.bb.injection.model.User;
 import com.bb.injection.util.ConnectionUtil;
 
@@ -15,6 +17,11 @@ import java.util.Optional;
  * Created by ankit on 30/5/16.
  */
 public class AddressDao {
+
+    private static AddressDao addressDao=new AddressDao();
+    public static AddressDao getInstance(){
+        return addressDao;
+    }
 
     public List<Long> getAddressId() {
         List<Long> addressIds = new ArrayList<>();
@@ -55,6 +62,44 @@ public class AddressDao {
         }
 
     }
+        //TODO offset in JDBC
+    public List<Address> getAddresses(){
+        List<Address> addresses=new ArrayList<>();
+        StringBuilder query=new StringBuilder();
+        query.append("select addressId,userId,street1,street2,street3,city,zip,r.name as regionName,c.name as countryName");
+        query.append(" from Address a join Region r on a.regionId=r.regionId join Country c on c.countryId=r.countryId");
+        query.append(" limit ").append(Constants.TOTAL_HITS_ALLOWED);
+
+        try(Connection connection=ConnectionUtil.getConnection();
+            Statement   statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query.toString())) {
+            while (rs.next()){
+                Address address=new Address();
+                address.setAddressId(rs.getLong("addressId"));
+                User user=new User();
+                user.setUserId(rs.getLong("userId"));
+                address.setUser(user);
+                address.setStreet1(rs.getString("street1"));
+                address.setStreet2(rs.getString("street2"));
+                address.setStreet3(rs.getString("street3"));
+                address.setCity(rs.getString("city"));
+                Region region=new Region();
+                region.setName(rs.getString("regionName"));
+                address.setRegion(region);
+                Country country=new Country();
+                region.setCountry(country);
+
+                addresses.add(address);
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return addresses;
+    }
+
+
 
 
 
